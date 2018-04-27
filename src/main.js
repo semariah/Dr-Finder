b0cb61e8812820f1e169c73555f4c879e31490ed
semhar <semaria.eyzac@gmal.com>
2018-04-27 16:42:25 -0700

    update and finalize for loop

--- src/main.js
+++ src/main.js
@@ -22,17 +22,17 @@ $(document).ready(function(){
       $('.medical-Issue').text(medIssue);
       //console.log(bodyArray[2]);
       if (wholeArray === 0){
-        $('.showsolution').text("0 Doctots Found");
+        $('.showsolution').text("0 Doctors meet your search criteria");
       } else {
         for (let i = 0; i < wholeArray.length; i++){
           $('.showsolution').append(`<h4>${wholeArray[i].profile.first_name} ${wholeArray[i].profile.last_name}</h4>`);
           $('.showsolution').append(`<li> Address: ${wholeArray[i].practices[0].visit_address.city}, ${wholeArray[i].practices[0].visit_address.state}, ${wholeArray[i].practices[0].visit_address.street}, ${wholeArray[i].practices[0].visit_address.zip}</li>`);
-          $('.showsolution').append(`<li> Phone Number: ${wholeArray[i].practices[0].phones[0].number}, ${wholeArray[i].practices[0].phones[0].type}`)
-
+          $('.showsolution').append(`<li> Phone Number: ${wholeArray[i].practices[0].phones[0].number}, ${wholeArray[i].practices[0].phones[0].type}`);
+          $('.showsolution').append(`<li> Accepts New Patients : ${wholeArray[i].practices[0].accepts_new_patients}</li>`);
+          $('.showsolution').append(`${wholeArray[i].practices[0].website}`);
         }
 
       }
-
     }, function(error){
       $('.showErrors').text(`there was an error processing your request: ${error.message}`);
     });
