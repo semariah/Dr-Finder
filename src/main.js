aee89097edd2d8957cd06ecd077f8ccd08a4cedf
semhar <semaria.eyzac@gmal.com>
2018-04-27 11:08:25 -0700

    update the files and experiment on the output of the name for check purposes of the import-export processes between the Api.js and the main.js files

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
+import {DocApi} from './Api.js';
 
 $(document).ready(function(){
   $("#doctor-form").submit(function(event){
