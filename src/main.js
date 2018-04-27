d24ca034dbd633bd2673c5228ce8f33cb3fa9a45
semhar <semaria.eyzac@gmal.com>
2018-04-27 10:46:50 -0700

    create the promise function

--- src/main.js
+++ src/main.js
@@ -9,6 +9,16 @@ $(document).ready(function(){
     event.preventDefault();
     let name = $("#name").val();
     let medIssue = $("#med-issue").val();
+    //$("#med-issue").val("");
+    let api = new DocApi();
+    let promise = api.docInfo();
+    promise.then(function(response){
+      let body = JSON.parse(response);
+      $('#showname').text(name);
+    }, function(error){
+      $('.showErrors').text(`there was an error processing your request: ${error.message}`);
+    });
 
-  })
-})
+
+  });
+});
