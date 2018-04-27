cd413b5374df125275d7232d016c6aa9c461cd90
semhar <semaria.eyzac@gmal.com>
2018-04-27 11:05:52 -0700

    add the class and constructor DocApi function

---
+++ src/Api.js
@@ -0,0 +1,21 @@
+class DocApi {
+  constructor (){}
+
+  docInfo(){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=d8d7a517e2d66913d48a9f290649e047
+`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+export {DocApi}
