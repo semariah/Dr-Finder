8788d1599c44345f2805fc00e69f743318ff3841
semhar <semaria.eyzac@gmal.com>
2018-04-30 18:49:39 -0700

    update the rest

--- src/main.js
+++ src/main.js
@@ -26,8 +26,11 @@ $(document).ready(function(){
           $('.showsolution').append(`<li> Address: ${wholeArray[i].practices[0].visit_address.city}, ${wholeArray[i].practices[0].visit_address.state}, ${wholeArray[i].practices[0].visit_address.street}, ${wholeArray[i].practices[0].visit_address.zip}</li>`);
           $('.showsolution').append(`<li> Phone Number: ${wholeArray[i].practices[0].phones[0].number}, ${wholeArray[i].practices[0].phones[0].type}`);
           $('.showsolution').append(`<li> Accepts New Patients : ${wholeArray[i].practices[0].accepts_new_patients}</li>`);
-          if ()
-          $('.showsolution').append(`${wholeArray[i].practices[0].website}`);
+          if (wholeArray[i].practices[0].website === undefined){
+            $('.showsolution').append(`Website: None`);
+          } else {
+            $('.showsolution').append(`${wholeArray[i].practices[0].website}`);
+          }
         }
       }
     }, function(error){
