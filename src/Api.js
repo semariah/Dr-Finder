694484b478b63026118dc91b21a222b4fb46d381
semhar <semaria.eyzac@gmal.com>
2018-04-30 18:48:35 -0700

    fix the Api and use the variable key provided

--- src/Api.js
+++ src/Api.js
@@ -4,7 +4,7 @@ class DocApi {
   docInfo(){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=d8d7a517e2d66913d48a9f290649e047`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
